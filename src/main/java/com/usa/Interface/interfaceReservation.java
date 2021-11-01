a0f6476ab061bcf0d47a41a6fcbe4af0cce63bfa
takashi <el3del446@outlook.com>
2021-10-31 20:04:52 -0500

    Reparacion de reto de mierda

--- src/main/java/com/usa/Interface/interfaceReservation.java
+++ src/main/java/com/usa/Interface/interfaceReservation.java
@@ -10,12 +10,13 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface interfaceReservation extends CrudRepository<Reservation, Integer>{
     
-    @Query("SELECT c.client, COUNT(c.client) FROM Reservation AS c group by c.client order by COUNT(c.client) DESC")
-    public List<Object[]> countTotalReservationByStartDate();
+    //JPQL
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationByClient();
 
+    //QUERY METHODS!
     public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
 
-    public List<Reservation> findAllByStatus(String status);
 
-    public List<Object[]> countTotalReservationByClient();
+    public List<Reservation> findAllByStatus(String status);
 }
